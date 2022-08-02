245fac0a7797f20f41d47fae5bc559f3b289bc1b
diego3008 <diegoarlechuga@gmail.com>
2022-08-02 10:09:48 -0500

    .

--- sliding_window/fruit_into_baskets.rb
+++ sliding_window/fruit_into_baskets.rb
@@ -1,5 +1,30 @@
 def total_fruit(trees)
-    
+    start = 0
+    max = 0
+    basket = {}
+    for i in 0...trees.length
+        if basket.length < 2 && !basket.key?(trees[i])
+            basket.store(trees[i], true)
+            max = [max, i - start + 1].max
+        elsif basket.key?(trees[i])
+            max = [max, i - start + 1].max
+        else
+            puts basket
+            basket = Hash.new(0)
+            basket.store(trees[i-1], true)
+            basket.store(trees[i], true)
+            start = i - 1
+
+            while trees[start] == trees[start-1]
+                start -= 1
+            end
+            max = [max, i - start + 1].max
+        end
+    end
+
+    max
+
 end
 
-puts total_fruit([1,2,3,2,2])
+puts total_fruit([1,2,3,2,2]
+)
